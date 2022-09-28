00fe7f710bb20372c65a876a9745c6414f45f82c
Artem IG <ortemeo@gmail.com>
2022-09-28 23:01:38 +0300

    upd

---
+++ src/main/kotlin/io/github/rtmigo/jasecache/TempDirs.kt
@@ -0,0 +1,20 @@
+package io.github.rtmigo.jasecache
+
+import java.nio.file.Path
+
+private fun isCorrectId(text: String) =
+    text.matches("^\\w{1,20}$".toRegex())
+
+class IllegalIdException : IllegalArgumentException(
+    "The id must be non-empty alphanumeric ASCII string " +
+        "up to 20 characters long.")
+
+fun toTempSubdir(id: String): Path {
+    if (!isCorrectId(id))
+        throw IllegalIdException()
+    val parent = systemTempDir()
+    val result = parent.resolve("jfc_$id")
+    if (result.parent != parent)
+        throw Error("The created directory $result is not a child of $parent")
+    return result
+}
